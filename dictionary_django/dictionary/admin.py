fcbd347e0f0ba1243622daee4d1d80c635f3bd10
thntranhongnhung <thntranhongnhung@gmail.com>
2023-01-04 20:49:41 +0700

    add file

--- dictionary_django/dictionary/admin.py
+++ dictionary_django/dictionary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 from .models import dictionary
 admin.site.register(dictionary)
+class DictionsryAdmin(admin.ModelAdmin):
+    list_display = ("word")
+
+admin.site.register(dictionary,DictionsryAdmin)
--- dictionary_django/dictionary/models.py
+++ dictionary_django/dictionary/models.py
@@ -5,3 +5,6 @@ class dictionary(models.Model):
     des = models.CharField(max_length=255)
     example= models.CharField(max_length=255,null=True)
 
+    def __str__(self):
+        return f"{self.word} "
+ 
