095dc4b66a60dc57040af303eb46ff0862d97640
thntranhongnhung <thntranhongnhung@gmail.com>
2023-01-06 07:29:42 +0700

    add file

--- dictionary_django/dictionary/admin.py
+++ dictionary_django/dictionary/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import dictionary
-admin.site.register(dictionary)
-class DictionsryAdmin(admin.ModelAdmin):
-    list_display = ("word")
 
-admin.site.register(dictionary,DictionsryAdmin)
+class DictionaryAdmin(admin.ModelAdmin):
+    list_display = ("word",)
+
+admin.site.register(dictionary,DictionaryAdmin)
